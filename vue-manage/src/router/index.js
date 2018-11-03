649e33dfbb100497138e15958ffde4208df43f6f
Your Name <1196044459@qq.com>
2018-11-03 23:00:44 +0800

    部部分

--- vue-manage/src/router/index.js
+++ vue-manage/src/router/index.js
@@ -1,15 +1,30 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/dashboard'
+    },
+    {
+      path: '/',
+      component: resolve => require(['../components/common/Home.vue'], resolve),
+      meta: {title: '自述文件'},
+      children: [
+        {
+          path: '/dashboard',
+          component: resolve => require(['../components/page/Dashboard.vue'], resolve),
+          meta: {title: '系统首页'}
+        },
+        {
+          path: '/icon',
+          component: resolve => require(['../components/page/Icon.vue'], resolve),
+          meta: {title: '自定义图标'}
+        }]
     }
-  ]
+    ]
 })
+
